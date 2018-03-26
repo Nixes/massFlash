82b57d93e884902512d212a95c51ede19b1a61b1
Callum Bryant <callum.l.bryant@gmail.com>
2018-03-26 11:48:15 +1100

    add dd based writer

--- flash.py
+++ flash.py
@@ -1,4 +1,4 @@
-import sys
+import os
 import threading
 import time
 from subprocess import call
@@ -6,6 +6,7 @@ from tqdm import tqdm
 
 class Flash:
     disk_path = ""
+    partition_path = ""
     image_path = ""
     thread_handle = None
     bytes_to_write = 0
@@ -22,13 +23,29 @@ class Flash:
         return filesize
 
 
-    def __init__(self, disk, image):  # {
+    def __init__(self, disk, image, partition_path):  # {
         self.disk_path = disk
         self.image_path = image
+        self.partition_path = partition_path
         # get the size of the image to write for percentage calculations
         # self.bytes_to_write = os.path.getsize(image)
         self.bytes_to_write = self.getFileSize(image)
         self.progress_bar = tqdm(total=self.bytes_to_write, desc="Disk: "+self.disk_path)
+    # }
+
+
+    # wrapper around dd command, until I can figure out why a bit for bit copy is not sufficient
+    def _ddWriteFileToDisk(self, sourceimage_path,  destdisk_path):  # {
+        # unmount disks
+        call(["sudo", "umount", "-f", self.partition_path])
+
+        self.bytes_written = self.bytes_to_write/2
+        # run dd
+        commands = ["sudo", "dd", "if="+sourceimage_path, "of="+destdisk_path, "bs=1m"]
+        print(commands)
+        call(commands)
+        self.bytes_written = self.bytes_to_write
+        print("Thread finished!")
     # }
 
     # disk_path = /dev/sda, image_path = ~/imagingtest.dd
@@ -58,7 +75,7 @@ class Flash:
     # may need rb+ for disk (+ does update mode), there are some indications that block devices can only be written to in this mode (especially in windows)
     def _writeFileToDisk(self, sourceimage_path,  destdisk_path):  # {
         # unmount drive
-        self.unmountDrive(destdisk_path)
+        call(["sudo", "umount", "-f", destdisk_path])
 
         with open(sourceimage_path, 'rb') as image:
             with open(destdisk_path, 'rb+') as disk:  # was wb
@@ -81,7 +98,7 @@ class Flash:
 
     # simple wrapper that starts image flash in a thread
     def writeFileToDisk(self,sourceimage_path,  destdisk_path):
-        thread_handle = threading.Thread(target=self._writeFileToDisk, args=[sourceimage_path, destdisk_path])
+        thread_handle = threading.Thread(target=self._ddWriteFileToDisk, args=[sourceimage_path, destdisk_path])
         thread_handle.start()
         self.thread_handle = thread_handle
 
